52f363572f6d9a201288854830ee2e39e56f366b
rmirpuri <rajiv.mirpuri@gmail.com>
2024-01-28 22:38:04 -0500

    Lecture 16: Stereotype annotations - @controller @service @repository

---
+++ learn-spring-framework/src/main/java/com/spring/core/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.spring.core.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DemoController {
+
+    public String hello(){
+
+        return "hellow controller";
+    }
+
+}
