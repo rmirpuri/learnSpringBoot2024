27dbfb1c0e0e341268e3c14589c56728b78fbbf3
rmirpuri <rajiv.mirpuri@gmail.com>
2024-02-02 22:39:38 -0500

    Lecture 30: initMethod and destroy methods

--- rajivTestError/src/main/java/com/spring/core/beans/BeansAnnotationDemo.java
+++ rajivTestError/src/main/java/com/spring/core/beans/BeansAnnotationDemo.java
@@ -16,6 +16,14 @@ class Students{
         System.out.println("Student class method called");
         address.print();
     }
+    public void init(){
+        System.out.println("Initialzation logic");
+    }
+
+    public void destroy(){
+        System.out.println("Desctruction logic");
+    }
+
 }
 
 class Address{
@@ -29,7 +37,7 @@ class AppConfig{
     public Address address(){
         return new Address();
     }
-    @Bean(name = {"studentBean","studentDemo"})
+    @Bean(name = "studentBean", initMethod = "init", destroyMethod = "destroy") //@Bean annotation tells the container to iniatilze and destroy wheneter it starts to manage object of type Student.
     public Students student(){
         return new Students(address());
     }
@@ -37,16 +45,30 @@ class AppConfig{
 
 public class BeansAnnotationDemo {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        //Students students = applicationContext.getBean(Students.class);
+
+        try (var applicationContext = new AnnotationConfigApplicationContext(AppConfig.class)){
+            //Students students = applicationContext.getBean(Students.class);
+            //Students students = (Students) applicationContext.getBean("studentBean");
+            Students students = (Students) applicationContext.getBean("studentBean");
+
+            String[] beanNames = applicationContext.getBeanDefinitionNames();
+            for(String bean: beanNames){
+                System.out.println(bean);
+            }
+
+            students.print();
+        }
+
+        //ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        /*//Students students = applicationContext.getBean(Students.class);
         //Students students = (Students) applicationContext.getBean("studentBean");
-        Students students = (Students) applicationContext.getBean("studentDemo");
+        Students students = (Students) applicationContext.getBean("studentBean");
 
         String[] beanNames = applicationContext.getBeanDefinitionNames();
         for(String bean: beanNames){
             System.out.println(bean);
         }
 
-        students.print();
+        students.print();*/
     }
 }
